8269fc2cded55eb8324755a8f959a4ba1dda3e1e
Didier Osindero <didierosindero@gmail.com>
2022-12-20 17:39:54 +0000

    Functioning navbar with w3 schools template

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,19 +2,14 @@ import { Link } from "react-router-dom";
 
 export const NavBar = (): JSX.Element => {
   return (
-    <ul className="navBarList">
-      <li>
-        <Link to="./" className="navBarItem">
-          Home
-        </Link>
-      </li>
-      <div className="dropdown">
-        <li>
-          <Link to="./posts" className="navBarItem">
+    <ul className="navBarListTest">
+      <li className="dropdown">
+        <div className="dropbtn">
+          <Link to="./posts" className="navBarItemTest">
             Posts
           </Link>
-        </li>
-        <div className="dropdownContent">
+        </div>
+        <div className="dropdown-content">
           <Link to="./posts/thought" className="dropdownItem">
             Thought
           </Link>
@@ -25,17 +20,45 @@ export const NavBar = (): JSX.Element => {
             Art
           </Link>
         </div>
-      </div>
-      <li>
-        <Link to="./write" className="navBarItem">
-          Write
-        </Link>
-      </li>
-      <li>
-        <Link to="./profile" className="navBarItem">
-          Profile
-        </Link>
       </li>
     </ul>
+    // <ul className="navBarList">
+    //   <li>
+    //     <Link to="./" className="navBarItem">
+    //       Home
+    //     </Link>
+    //   </li>
+    //   <div className="dropdown">
+    //     <Link to="./posts" className="navBarItem">
+    //       Posts
+    //     </Link>
+    //     {/* <div className="dropdownContent">
+    //       <Link to="./posts/thought" className="dropdownItem">
+    //         Thought
+    //       </Link>
+    //       <Link to="./posts/science" className="dropdownItem">
+    //         Science
+    //       </Link>
+    //       <Link to="./posts/art" className="dropdownItem">
+    //         Art
+    //       </Link>
+    //     </div> */}
+    //     <div className="dropdownContent">
+    //       <div className="dropdownItem">Flags</div>
+    //       <div className="dropdownItem">Capitals</div>
+    //       <div className="dropdownItem">Population</div>
+    //     </div>
+    //   </div>
+    //   <li>
+    //     <Link to="./write" className="navBarItem">
+    //       Write
+    //     </Link>
+    //   </li>
+    //   <li>
+    //     <Link to="./profile" className="navBarItem">
+    //       Profile
+    //     </Link>
+    //   </li>
+    // </ul>
   );
 };
