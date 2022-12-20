2ec60b2c98b7cb1800d36ce84e8e8f40e4ed1c2c
Didier Osindero <didierosindero@gmail.com>
2022-12-20 15:50:29 +0000

    Nest routes for thought art and science

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,13 +3,21 @@ import { PostsPage } from "./pages/PostsPage";
 import { Route, Routes } from "react-router-dom";
 import { ProfilePage } from "./pages/ProfilePage";
 import { WritePage } from "./pages/WritePage";
+import { ThoughtPage } from "./pages/posts-pages/ThoughtPage";
+import { ArtPage } from "./pages/posts-pages/ArtPage";
+import { SciencePage } from "./pages/posts-pages/SciencePage";
 
 export const MainContent = (): JSX.Element => {
   return (
     <div className="mainContentWrapper">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts">
+          <Route index element={<PostsPage />} />
+          <Route path="thought" element={<ThoughtPage />} />
+          <Route path="art" element={<ArtPage />} />
+          <Route path="science" element={<SciencePage />} />
+        </Route>
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/write" element={<WritePage />} />
       </Routes>
