a641a687203807752f5bd2449f1b296556df7db9
Didier Osindero <didierosindero@gmail.com>
2022-12-20 14:47:49 +0000

    Add routing for Write and Profile pages

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,8 @@
 import { HomePage } from "./pages/HomePage";
 import { PostsPage } from "./pages/PostsPage";
 import { Route, Routes } from "react-router-dom";
+import { ProfilePage } from "./pages/ProfilePage";
+import { WritePage } from "./pages/WritePage";
 
 export const MainContent = (): JSX.Element => {
   return (
@@ -8,6 +10,8 @@ export const MainContent = (): JSX.Element => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/posts" element={<PostsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/write" element={<WritePage />} />
       </Routes>
     </div>
   );
