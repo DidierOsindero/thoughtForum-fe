afd5b31f936974fd6fc1a5b635a8590371f9d039
Didier Osindero <didierosindero@gmail.com>
2022-12-20 14:43:48 +0000

    Add routing for HomePage and PostsPage

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,14 @@
 import { HomePage } from "./pages/HomePage";
 import { PostsPage } from "./pages/PostsPage";
+import { Route, Routes } from "react-router-dom";
 
 export const MainContent = (): JSX.Element => {
   return (
     <div className="mainContentWrapper">
-      <HomePage />
-      <PostsPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posts" element={<PostsPage />} />
+      </Routes>
     </div>
   );
 };
