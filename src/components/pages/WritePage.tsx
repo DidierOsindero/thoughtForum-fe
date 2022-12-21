d5804e2182233291514e52b4a2478bb864ea222e
Didier Osindero <didierosindero@gmail.com>
2022-12-21 12:30:44 +0000

    Add and style 'choose image' upload

--- src/components/pages/WritePage.tsx
+++ src/components/pages/WritePage.tsx
@@ -3,10 +3,22 @@ export const WritePage = (): JSX.Element => {
     <div className="WritePageContainer">
       <div className="leftOfPage">
         <div className="createPostContainer">
+          <form>
+            <label className="chooseIMG submitButton" htmlFor="img">
+              Choose an image
+            </label>
+            <input
+              className="createPostInputIMG"
+              type="file"
+              id="img"
+              name="img"
+              accept="image/*"
+            ></input>
+          </form>
           <img
             src="https://images.unsplash.com/photo-1553782749-5ab8693a5f4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hha2VzcGVhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
             alt=""
-            className="featuredPostIMG"
+            className="createPostIMG"
           />
           <div className="featuredPostTitle">
             <h3>Hubris in Hamlet</h3>
@@ -17,11 +29,7 @@ export const WritePage = (): JSX.Element => {
                 required
               ></textarea>
               <br />
-              <input
-                className="submitCreatedPost"
-                type="submit"
-                value="✅ Post"
-              />
+              <input className="submitButton" type="submit" value="✅ Post" />
             </form>
           </div>
         </div>
