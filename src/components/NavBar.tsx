c2c17f6a0f8f394daaa20181465e2ef108682b4b
Didier Osindero <didierosindero@gmail.com>
2022-12-21 09:40:35 +0000

    Add fixed CSS position to navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,41 +2,44 @@ import { Link } from "react-router-dom";
 
 export const NavBar = (): JSX.Element => {
   return (
-    <ul className="navBarList">
-      <li className="navBarListItem">
-        <Link to="./" className="navBarItemLink">
-          <div className="navBarItemText">Home</div>
-        </Link>
-      </li>
-      <li className="dropdown">
-        <Link to="./posts" className="navBarItemLink">
-          <div className="dropbtn">Posts</div>
-        </Link>
-        <div className="dropdownContent">
-          <Link to="./posts/thought" className="dropdownItem">
-            Thought
+    <>
+      <ul className="navBarList">
+        <li className="navBarListItem">
+          <Link to="./" className="navBarItemLink">
+            <div className="navBarItemText">Home</div>
           </Link>
-          <Link to="./posts/science" className="dropdownItem">
-            Science
+        </li>
+        <li className="dropdown">
+          <Link to="./posts" className="navBarItemLink">
+            <div className="dropbtn">Posts</div>
           </Link>
-          <Link to="./posts/art" className="dropdownItem">
-            Art
+          <div className="dropdownContent">
+            <Link to="./posts/thought" className="dropdownItem">
+              Thought
+            </Link>
+            <Link to="./posts/science" className="dropdownItem">
+              Science
+            </Link>
+            <Link to="./posts/art" className="dropdownItem">
+              Art
+            </Link>
+          </div>
+        </li>
+        <li className="navBarListItem">
+          <Link to="./write" className="navBarItemLink">
+            <div className="navBarItemText">Write</div>
           </Link>
-        </div>
-      </li>
-      <li className="navBarListItem">
-        <Link to="./write" className="navBarItemLink">
-          <div className="navBarItemText">Write</div>
-        </Link>
-      </li>
-      <li className="navBarListItem">
-        <Link to="./profile" className="navBarItemLink">
-          <div className="navBarItemText">Profile</div>
-        </Link>
-      </li>
-      <li className="navBarListItemRight">
-        <div className="navBarItemText">Sign-In</div>
-      </li>
-    </ul>
+        </li>
+        <li className="navBarListItem">
+          <Link to="./profile" className="navBarItemLink">
+            <div className="navBarItemText">Profile</div>
+          </Link>
+        </li>
+        <li className="navBarListItemRight">
+          <div className="navBarItemText">Sign-In</div>
+        </li>
+      </ul>
+      <div className="spaceBelowNavBar"></div>
+    </>
   );
 };
