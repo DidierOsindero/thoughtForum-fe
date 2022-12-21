bf5e3bdaf098ddf1e11d4ff7a3bc2e1874af4e58
Didier Osindero <didierosindero@gmail.com>
2022-12-21 19:01:49 +0000

    Seperate CSS for posts and featured posts

--- src/components/pages/PostsPage.tsx
+++ src/components/pages/PostsPage.tsx
@@ -1,17 +1,17 @@
 export const PostsPage = (): JSX.Element => {
   return (
-    <div className="PostsPageContainer">
-      <div className="featuredPostsContainer">
-        <div className="featuredPost">
+    <div className="postsPageContainer">
+      <div className="postsContainer">
+        <div className="post">
           <img
             src="https://images.unsplash.com/photo-1617791160536-598cf32026fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80"
             alt=""
-            className="featuredPostIMG"
+            className="postIMG"
           />
-          <div className="featuredPostTitle">
+          <div className="postTitle">
             <h3>Why do we think the way we do?</h3>
 
-            <p className="featuredPostContent">
+            <p className="postContent">
               In their most common sense, the terms thought and thinking refer
               to conscious cognitive processes that can happen independently of
               sensory stimulation. Their most paradigmatic forms are judging,
@@ -23,16 +23,16 @@ export const PostsPage = (): JSX.Element => {
           </div>
         </div>
 
-        <div className="featuredPost">
+        <div className="post">
           <img
             src="https://images.unsplash.com/photo-1553782749-5ab8693a5f4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hha2VzcGVhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
             alt=""
-            className="featuredPostIMG"
+            className="postIMG"
           />
-          <div className="featuredPostTitle">
+          <div className="postTitle">
             <h3>Hubris in Hamlet</h3>
 
-            <p className="featuredPostContent">
+            <p className="postContent">
               The Tragedy of Hamlet, Prince of Denmark, often shortened to
               Hamlet (/ˈhæmlɪt/), is a tragedy written by William Shakespeare
               sometime between 1599 and 1601. It is Shakespeare's longest play,
