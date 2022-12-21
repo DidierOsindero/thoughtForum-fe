1022971b5968369ed76e0255714e8afb2da7d5e0
Didier Osindero <didierosindero@gmail.com>
2022-12-21 15:35:28 +0000

    Move post settings to right side of page/add radio

--- src/components/pages/WritePage.tsx
+++ src/components/pages/WritePage.tsx
@@ -12,59 +12,74 @@ export const WritePage = (): JSX.Element => {
 
   return (
     <div className="WritePageContainer">
-      <div className="leftOfPage">
-        <div className="createPostContainer">
-          <div className="previewImageContainer">
-            {imageURL && selectedImage && (
-              <>
-                <img
-                  className="createPostIMG"
-                  src={imageURL}
-                  alt={selectedImage.name}
-                  height="100px"
-                />
-              </>
-            )}
-            {!(imageURL && selectedImage) && (
-              <>
-                <img
-                  className="createPostIMG"
-                  src="https://images.unsplash.com/photo-1635352723068-ffb3b922397f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGluc2VydCUyMGltYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-                  alt=""
-                  height="100px"
-                />
-              </>
-            )}
-          </div>
-          <label className="chooseIMG submitButton" htmlFor="img">
-            {imageURL && selectedImage && <span>Change image</span>}
-            {!(imageURL && selectedImage) && <span>Choose an image</span>}
-          </label>
-          <input
-            className="createPostInputIMG"
-            type="file"
-            id="img"
-            name="img"
-            accept="image/*"
-            onChange={(e) =>
-              setSelectedImage(e.target.files ? e.target.files[0] : null)
-            }
-          ></input>
-          <div className="featuredPostTitle">
-            <h3>Hubris in Hamlet</h3>
-            <form>
+      <form>
+        <div className="leftOfPage">
+          <div className="createPostContainer">
+            <div className="previewImageContainer">
+              {imageURL && selectedImage && (
+                <>
+                  <img
+                    className="createPostIMG"
+                    src={imageURL}
+                    alt={selectedImage.name}
+                    height="100px"
+                  />
+                </>
+              )}
+              {!(imageURL && selectedImage) && (
+                <>
+                  <img
+                    className="createPostIMG"
+                    src="https://images.unsplash.com/photo-1635352723068-ffb3b922397f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGluc2VydCUyMGltYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                    alt=""
+                    height="100px"
+                  />
+                </>
+              )}
+            </div>
+            <div className="featuredPostTitle">
+              <h3>Hubris in Hamlet</h3>
+
               <textarea
                 className="createPostTextInput"
                 placeholder="Write here..."
                 required
               ></textarea>
-              <br />
-              <input className="submitButton" type="submit" value="✅ Post" />
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="rightOfPage"> </div>
+        <div className="rightOfPage">
+          <div className="createPostSettingsContainer">
+            <label className="chooseIMG submitButton" htmlFor="img">
+              {imageURL && selectedImage && <span>Change image</span>}
+              {!(imageURL && selectedImage) && <span>Choose an image</span>}
+            </label>
+            <input
+              className="createPostInputIMG"
+              type="file"
+              id="img"
+              name="img"
+              accept="image/*"
+              onChange={(e) =>
+                setSelectedImage(e.target.files ? e.target.files[0] : null)
+              }
+            ></input>
+            <br />
+            <input type="radio" name="postTypeRadio" value="thought" required />
+            <label htmlFor="postTypeRadio">Thought</label>
+            <br />
+            <input type="radio" name="postTypeRadio" value="science" />
+            <label htmlFor="postTypeRadio"> Science</label>
+            <br />
+            <input type="radio" name="postTypeRadio" value="art" />
+            <label htmlFor="postTypeRadio"> Art</label>
+            <br />
+            <div>
+              <input className="submitButton" type="submit" value="✅ Post" />
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
