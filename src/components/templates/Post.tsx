29621a99c48e9e6b857eca0d835f3906a5705d11
Didier Osindero <didierosindero@gmail.com>
2022-12-22 13:40:35 +0000

    Add date and userName to posts

--- src/components/templates/Post.tsx
+++ src/components/templates/Post.tsx
@@ -8,10 +8,18 @@ export function Post({ postData }: IPostProps): JSX.Element {
   return (
     <div className="post">
       <img src={postData.img} alt="" className="postIMG" />
+      <div className="postDetails">
+        <p className="postUserName">
+          <i>Kamasi James</i>
+        </p>
+      </div>
       <div className="postTitle">
         <h3>{postData.title}</h3>
         <p className="postContent">{postData.content}</p>
       </div>
+      <p className="postDate">
+        <i>{postData.date}</i>
+      </p>
     </div>
   );
 }
