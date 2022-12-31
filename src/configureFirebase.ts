bf7c8073ef9bba57b2ca5b40f6f7da37537e4297
Didier Osindero <didierosindero@gmail.com>
2022-12-31 09:34:20 +0000

    Add Firebase and config file

---
+++ src/configureFirebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf0AKMQhlgfFe_xrRz6pM5UYj9YGYtM78",
+  authDomain: "thought-forum-98eaf.firebaseapp.com",
+  projectId: "thought-forum-98eaf",
+  storageBucket: "thought-forum-98eaf.appspot.com",
+  messagingSenderId: "572040641131",
+  appId: "1:572040641131:web:dc5cae4cb13a81897d0f2e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
