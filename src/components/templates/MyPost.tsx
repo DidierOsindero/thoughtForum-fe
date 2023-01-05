4f182ac24f66be5c50e5e7899190a9c1024c361c
Didier Osindero <didierosindero@gmail.com>
2023-01-05 08:00:26 +0000

    Add functioning delete button to my posts

--- src/components/templates/MyPost.tsx
+++ src/components/templates/MyPost.tsx
@@ -1,4 +1,5 @@
-import { IPostData } from "../../App";
+import axios from "axios";
+import { BASE_URL, IPostData } from "../../App";
 
 export interface IMyPostProps {
   postData: IPostData;
@@ -7,6 +8,13 @@ export interface IMyPostProps {
 export function MyPost({ postData }: IMyPostProps): JSX.Element {
   const indexOfT = postData.creation_date.indexOf("T");
   const postDate = postData.creation_date.slice(0, indexOfT);
+
+  const handleDeletePost = async (postId: number) => {
+    const response = await axios.delete(
+      BASE_URL + "profile/posts?postid=" + postId
+    );
+    console.log("Deleted Post:", response.data);
+  };
   return (
     <div className="post">
       <img src={postData.img} alt="" className="postIMG" />
@@ -16,8 +24,19 @@ export function MyPost({ postData }: IMyPostProps): JSX.Element {
       <div className="postTitle">
         <h3>{postData.title}</h3>
       </div>
+      <p>
+        <i>{postData.category}</i>
+      </p>
       <p className="postContent">{postData.content}</p>
-      <button>Edit</button>
+      <button
+        className="delete-post-btn"
+        onClick={() => handleDeletePost(postData.post_id)}
+      >
+        Delete
+      </button>
+      <p className="userPostPrivacy">
+        <i>{postData.privacy}</i>
+      </p>
     </div>
   );
 }
