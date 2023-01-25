127de3fed22752df40dbf9d3d59e00c52b844ff4
Didier Osindero <didierosindero@gmail.com>
2023-01-25 11:33:25 +0000

    Add loading spinner and format

--- src/components/pages/PostsPage.tsx
+++ src/components/pages/PostsPage.tsx
@@ -1,16 +1,17 @@
 import { PostsListView } from "../templates/PostsListView";
-//import { dummyData } from "../../utils/dummyPostData";
 import axios from "axios";
 import { BASE_URL, IPostData } from "../../App";
 import { useEffect, useState } from "react";
+import { Triangle } from "react-loader-spinner";
 
 export const PostsPage = (): JSX.Element => {
   const [postDataArray, setPostDataArray] = useState<IPostData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const getPostsData = async () => {
-    console.log("START getPostsData");
+    setIsLoading(true);
     const { data } = await axios.get(BASE_URL + "posts");
-    console.log("FINISH getPostsData");
+    setIsLoading(false);
     setPostDataArray(data);
   };
 
@@ -21,6 +22,18 @@ export const PostsPage = (): JSX.Element => {
   return (
     <div className="postsPageContainer">
       <h1 className="postsPageTitle">All Posts</h1>
+      {isLoading && (
+        <div className="ctn-loading-triangle">
+          <Triangle
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperClass="loading-triangle"
+            visible={true}
+          />
+        </div>
+      )}
       {postDataArray && <PostsListView postDataArray={postDataArray} />}
     </div>
   );
--- src/utils/filterPostsByCategory.test.ts
+++ src/utils/filterPostsByCategory.test.ts
@@ -5,7 +5,7 @@ import {
   dummyScienceData,
   dummyThoughtData,
 } from "./dummyPostData";
-test(" Given a category, filterPostsByCategory returns all posts which match the given category", () => {
+test("Given a category, filterPostsByCategory returns all posts which match the given category", () => {
   expect(filterPostsByCategory(dummyData, "all")).toStrictEqual(dummyData);
   expect(filterPostsByCategory(dummyData, "art")).toStrictEqual(dummyArtData);
   expect(filterPostsByCategory(dummyData, "science")).toStrictEqual(
