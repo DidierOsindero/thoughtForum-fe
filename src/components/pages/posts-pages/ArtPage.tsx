5b2a7dc2efcec12c0be6818d91e6b0c523cf30d3
Didier Osindero <didierosindero@gmail.com>
2023-01-26 11:16:35 +0000

    Add loading spinner to Art/Science / Thought pages

--- src/components/pages/posts-pages/ArtPage.tsx
+++ src/components/pages/posts-pages/ArtPage.tsx
@@ -2,11 +2,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { IPostData, BASE_URL } from "../../../App";
 import { PostsListView } from "../../templates/PostsListView";
+import { Triangle } from "react-loader-spinner";
 
 export const ArtPage = (): JSX.Element => {
   const [postDataArray, setPostDataArray] = useState<IPostData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const getPostsData = async () => {
+    setIsLoading(true);
     const { data } = await axios.get(BASE_URL + "posts/art");
+    setIsLoading(false);
     setPostDataArray(data);
   };
 
@@ -17,6 +22,18 @@ export const ArtPage = (): JSX.Element => {
   return (
     <div className="postsPageContainer">
       <h1 className="postsPageTitle">Art</h1>
+      {isLoading && (
+        <div className="ctn-loading-triangle">
+          <Triangle
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperClass="loading-triangle"
+            visible={true}
+          />
+        </div>
+      )}
       {postDataArray && <PostsListView postDataArray={postDataArray} />}
     </div>
   );
--- src/components/pages/posts-pages/ThoughtPage.tsx
+++ src/components/pages/posts-pages/ThoughtPage.tsx
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { IPostData, BASE_URL } from "../../../App";
-
 import { PostsListView } from "../../templates/PostsListView";
+import { Triangle } from "react-loader-spinner";
 
 export const ThoughtPage = (): JSX.Element => {
   const [postDataArray, setPostDataArray] = useState<IPostData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const getPostsData = async () => {
+    setIsLoading(true);
     const { data } = await axios.get(BASE_URL + "posts/thought");
+    setIsLoading(false);
     setPostDataArray(data);
   };
 
@@ -18,6 +22,18 @@ export const ThoughtPage = (): JSX.Element => {
   return (
     <div className="postsPageContainer">
       <h1 className="postsPageTitle">Thought</h1>
+      {isLoading && (
+        <div className="ctn-loading-triangle">
+          <Triangle
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperClass="loading-triangle"
+            visible={true}
+          />
+        </div>
+      )}
       {postDataArray && <PostsListView postDataArray={postDataArray} />}
     </div>
   );
