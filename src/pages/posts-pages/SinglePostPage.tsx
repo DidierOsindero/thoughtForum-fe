7b45eab20e36c4bfb7659a88050f0559b60f9101
Didier Osindero <didierosindero@gmail.com>
2023-02-20 16:25:33 +0000

    if user, do not show own posts in recommended

--- src/pages/posts-pages/SinglePostPage.tsx
+++ src/pages/posts-pages/SinglePostPage.tsx
@@ -1,13 +1,18 @@
 import axios from "axios";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { BASE_URL, IPostData } from "../../App";
 import { convertTimeStampToDate } from "../../utils/convertTimeStampToDate";
 import { RecommendedPost } from "../../components/RecommendedPost";
+import { User } from "firebase/auth";
+import { UserContext } from "../../context/UserContext";
 
 export const SinglePostPage = (): JSX.Element => {
   const { id } = useParams();
-
+  const { user } = useContext(UserContext) as {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  };
   const [postData, setPostData] = useState<IPostData>();
   const [recommendedPostData, setRecommendedPostData] = useState<IPostData[]>(
     []
@@ -18,15 +23,20 @@ export const SinglePostPage = (): JSX.Element => {
       const responseCurrentPost = await axios.get(BASE_URL + "posts/" + id);
       const currentPostData: IPostData = responseCurrentPost.data[0];
       setPostData(currentPostData);
+
+      const token = await user?.getIdToken();
+      const config = { headers: { Authorization: "Bearer " + token } };
+
       const responseRecommendedPosts = await axios.get(
-        BASE_URL + "posts/recommend/" + currentPostData.category + "/" + id
+        BASE_URL + "posts/recommend/" + currentPostData.category + "/" + id,
+        config
       );
       const recommendPostArr = responseRecommendedPosts.data;
       setRecommendedPostData(recommendPostArr);
     } catch (error) {
       console.error("There was an error fetching data for this page:", error);
     }
-  }, [id]);
+  }, [id, user]);
 
   useEffect(() => {
     getPostsAndRecommendedPostsData();
