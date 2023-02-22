2aa6f1e585704278d21a4ba1156c14a3b6b4e795
Didier Osindero <didierosindero@gmail.com>
2023-02-22 13:42:05 +0000

    Change MyPost to be more similar to regular post

--- src/components/MyPost.tsx
+++ src/components/MyPost.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { BASE_URL, IPostData } from "../App";
+import { convertTimeStampToDate } from "../utils/convertTimeStampToDate";
 
 export interface IMyPostProps {
   postData: IPostData;
@@ -20,28 +22,32 @@ export function MyPost({
 
   return (
     <div className="ctn-post">
-      <div className="ctn-post-img">
-        <img src={postData.img} alt="" className="post-img" />
-      </div>
-      <p className="post-date">
-        <i>{postDate}</i>
-      </p>
-      <div className="post-title">
-        <h3>{postData.title}</h3>
-      </div>
-      <p>
-        <i>{postData.category}</i>
-      </p>
-      <p className="post-content">{postData.content}</p>
-      <button
-        className="delete-post-btn"
-        onClick={() => handleDeletePost(postData.post_id)}
-      >
-        Delete
-      </button>
-      <p className="user-post-privacy">
-        <i>{postData.privacy}</i>
-      </p>
+      <Link to={"/posts/" + postData.post_id}>
+        <div className="ctn-post-img">
+          <img src={postData.img} alt="" className="post-img" />
+        </div>
+        <div className="postDetails">
+          <p className="postUserName">
+            <i>{postData.username && postData.username}</i>
+          </p>
+        </div>
+        <div className="post-title">
+          <h3>{postData.title}</h3>
+        </div>
+        <p className="post-content">{postData.content.slice(0, 200)}...</p>
+        <p className="post-date">
+          <i>{convertTimeStampToDate(postData.creation_date)}</i>
+        </p>
+        <button
+          className="delete-post-btn"
+          onClick={() => handleDeletePost(postData.post_id)}
+        >
+          Delete
+        </button>
+        <p className="user-post-privacy">
+          <i>{postData.privacy}</i>
+        </p>
+      </Link>
     </div>
   );
 }
