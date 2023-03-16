d01a64a811bfd2a0a86ab9e9d487c408522cd975
Didier Osindero <didierosindero@gmail.com>
2023-03-16 10:13:29 +0000

    Functioning comment submit

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,9 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { BASE_URL } from "../App";
+import { UserContext } from "../context/UserContext";
 import { convertTimeStampToDate } from "../utils/convertTimeStampToDate";
 import "./Comments.css";
 import { CommentsInput } from "./CommentsInput";
@@ -18,35 +21,84 @@ interface ICommentsData {
 }
 
 export const Comments = ({ postId }: CommentsProps): JSX.Element => {
-  const [commentsData, setCommentsData] = useState<ICommentsData[]>([]);
-  const getComments = async (postId: number) => {
-    const { data } = await axios.get(`${BASE_URL}posts/${postId}/comments`);
-    setCommentsData(data);
+  //GET user from context
+  const { user } = useContext(UserContext) as {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
   };
 
+  //Define Toast
+  const sharedCommentNotif = () => toast("You have shared a comment ✅");
+
+  //User input and comments states
+  const [userInput, setUserInput] = useState<string>("");
+  const [commentsData, setCommentsData] = useState<ICommentsData[]>([]);
+
+  //Fetch comments on mount
   useEffect(() => {
     getComments(postId);
   }, [postId]);
-  if (commentsData[0]) {
-    return (
-      <div className="ctn-comments">
-        {commentsData.map((comment) => {
-          return (
-            <div className="ctn-comment" key={comment.comment_id}>
-              <p>
-                <b className="comment-username">{comment.username}:</b>{" "}
-                {comment.comment}
-              </p>
-              <p className="comment-date">
-                {convertTimeStampToDate(comment.creation_date)}
-              </p>
-            </div>
-          );
-        })}
-        <CommentsInput />
-      </div>
-    );
-  } else {
-    return <CommentsInput />;
-  }
+
+  //Handler to submit comment
+  const handleSubmitComment = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await postComment(userInput);
+      sharedCommentNotif();
+      await getComments(postId);
+      setUserInput("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //GET request for comments
+  const getComments = async (postId: number) => {
+    const { data } = await axios
+      .get(`${BASE_URL}posts/${postId}/comments`)
+      .catch(function (error) {
+        console.error("There was an error getting the comments:");
+        throw error;
+      });
+    setCommentsData(data);
+  };
+
+  //POST request for comments
+  const postComment = async (commentText: string) => {
+    const token = await user?.getIdToken();
+    const config = { headers: { Authorization: "Bearer " + token } };
+    await axios
+      .post(
+        `${BASE_URL}posts/comments`,
+        { post_id: postId, commentText },
+        config
+      )
+      .catch(function (error) {
+        console.error("There was an error posting the comment:");
+        throw error;
+      });
+  };
+
+  return (
+    <div className="ctn-comments">
+      {commentsData.map((comment) => {
+        return (
+          <div className="ctn-comment" key={comment.comment_id}>
+            <p>
+              <b className="comment-username">{comment.username}:</b>{" "}
+              {comment.comment}
+            </p>
+            <p className="comment-date">
+              {convertTimeStampToDate(comment.creation_date)}
+            </p>
+          </div>
+        );
+      })}
+      <CommentsInput
+        userInput={userInput}
+        setUserInput={setUserInput}
+        handleSubmitComment={handleSubmitComment}
+      />
+    </div>
+  );
 };
--- src/components/CommentsInput.tsx
+++ src/components/CommentsInput.tsx
@@ -1,11 +1,39 @@
-export const CommentsInput = (): JSX.Element => {
-  return (
-    <div className="comment-input-and-button">
-      <textarea
-        className="comment-input"
-        placeholder="Share your thoughts..."
-      ></textarea>
-      <button className="upload-comment-btn">+</button>
-    </div>
-  );
+import { User } from "firebase/auth";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+interface CommentsInputProps {
+  userInput: string;
+  setUserInput: React.Dispatch<React.SetStateAction<string>>;
+  handleSubmitComment: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+}
+
+export const CommentsInput = ({
+  userInput,
+  setUserInput,
+  handleSubmitComment,
+}: CommentsInputProps): JSX.Element => {
+  //GET user from context
+  const { user } = useContext(UserContext) as {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  };
+
+  if (user) {
+    return (
+      <div className="comment-input-and-button">
+        <form onSubmit={(e) => handleSubmitComment(e)}>
+          <textarea
+            className="comment-input"
+            placeholder="Share your thoughts..."
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          ></textarea>
+          <button className="upload-comment-btn">+</button>
+        </form>
+      </div>
+    );
+  } else {
+    return <h5>Sign-in to share your thoughts</h5>;
+  }
 };
